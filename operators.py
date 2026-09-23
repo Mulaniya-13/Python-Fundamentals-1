32df165bd85b2a6d99dbea8c1f546722ee748566
Aashish Mulaniya <mulaniyaaashish@gmail.com>
2026-09-23 20:17:57 +0530

    logical

--- operators.py
+++ operators.py
@@ -31,3 +31,11 @@ print(a)
 a=10
 a**=2
 print(a)
+
+#Logical
+var=True
+print(not(var))
+print((5>3) and (3<6))#True
+print((5>3) and (3<2))#false
+print((2>4) or (3<5)) #true
+print((2>5) or (3<2)) #false 
